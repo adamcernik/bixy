30a3991779555fb48cec9ed931ebcbeb6e3df454
adamcernik <adam.cernik@gmail.com>
2025-05-19 22:55:59 +0200

    Clean up partners page related files

---
+++ app/components/ClientLayout.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { AppBar, Toolbar, Typography, Button, Container, Box } from '@mui/material';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { getAssetPath } from '../utils/pathUtils';
+
+export default function ClientLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+  const isPublicPage = pathname.startsWith('/products');
+
+  return (
+    <>
+      {isPublicPage ? null : null}
+      {children}
+    </>
+  );
+} 
